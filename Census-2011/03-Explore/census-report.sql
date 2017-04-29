d2c3abed5042a446a569c428234e824100646a69
Will Deakin <will.deakin@crinstitute.org.uk>
2017-04-29 23:12:36 +0100

    Add census-report.sql for generating aggregated census data

---
+++ Census-2011/03-Explore/census-report.sql
@@ -0,0 +1,16 @@
+drop table _tmp;
+
+select i.id,
+sum(i.population::integer) as population,
+sum(males::integer) as males,
+sum(females::integer) as females,
+sum(household::integer) as household,
+sum(communal_establishment::integer) as communal_establishment,
+sum(child::integer) as child,
+sum(cast(i.area_ha as double precision)) as area_ha
+into _tmp
+from table_gb_census_report_02 i
+group by i.id
+order by id;
+
+\copy _tmp to 'gb-census-report-03.tsv' with csv delimiter E'\t' header;
