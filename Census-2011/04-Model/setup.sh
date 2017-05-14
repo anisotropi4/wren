2d462105dfc88fb2f7ac613c89eb23e771a0b1b7
Will Deakin <will.deakin@crinstitute.org.uk>
2017-05-14 22:07:58 +0100

    set correct data type

--- Census-2011/04-Model/setup.sh
+++ Census-2011/04-Model/setup.sh
@@ -13,4 +13,4 @@ fi
 
 < gb-census-report.tsv csvjson -t | jq -c 'sort_by(.id)' | jq -c '.[]' > setup/gb-census-report.ndjson
 
-ndjson-join 'd.id' setup/PostalSector-areas.ndjson setup/gb-census-report.ndjson | ndjson-map '{id: d[0].id, area_m2: d[0].area_m2, area_ha: d[1].area_ha, population: d[1].population, density_ha: d[1].population / ((d[1].area_ha == 0) ? Math.ceil(d[0].area_m2 / 10000.0) : d[1].area_ha), pdensity_ha: Math.max(1.0, +d[1].population) / ((d[1].area_ha == 0) ? Math.ceil(d[0].area_m2 / 10000.0) : d[1].area_ha)}' > output/gb-census-report-density.ndjson
+ndjson-join 'd.id' setup/PostalSector-areas.ndjson setup/gb-census-report.ndjson | ndjson-map '{id: d[0].id, area_m2: +d[0].area_m2, area_ha: +d[1].area_ha, area: Math.ceil((d[1].area_ha == 0) ? (d[0].area_m2 / 10000.0) : +d[1].area_ha), population: +d[1].population, density_ha: +d[1].population / ((d[1].area_ha == 0) ? Math.ceil(d[0].area_m2 / 10000.0) : d[1].area_ha), pdensity_ha: Math.max(1.0, +d[1].population) / ((d[1].area_ha == 0) ? Math.ceil(d[0].area_m2 / 10000.0) : d[1].area_ha)}' > output/gb-census-report-density.ndjson
