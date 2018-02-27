92c4e04a817350a89bd7a8b9e61690fa61a90f42
Will Deakin <will.deakin@crinstitute.org.uk>
2018-02-27 08:45:44 +0000

    round locations to 5 d.p.

--- NaPTAN-Rail/04-Model-05-iNterpret/run.sh
+++ NaPTAN-Rail/04-Model-05-iNterpret/run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 
 # The count and reordering of fields can be found in the 'suniq.sh' script
 # in the <https://github.com/anisotropi4/goldfinch> repository
@@ -7,7 +7,7 @@ echo "Type	Count" > StopTypes.tsv
 < StopPoint.ndjson jq -c -r '.StopClassification.StopType' | sort - | uniq -c | sort -rn | sed 's/^ *\([0-9][0-9]*\) \(.*\)$/\2\t\1/' >> StopTypes.tsv
 
 
-< StopPoint.ndjson jq -c '{AtcoCode} + (.Descriptor | {CommonName, Street, Indicator} ) + (.Place.Location.Translation | {lat: .Latitude, lon: .Longitude}) + (.Place | {node: .NptgLocalityRef}) + (.StopClassification | {stoptype: .StopType}) | del(.[] | select(. == null))' > naptandata.ndjson
+< StopPoint.ndjson jq -c '{AtcoCode} + (.Descriptor | {CommonName, Street, Indicator} ) + (.Place.Location.Translation | {lat: (.Latitude | (tonumber + 0.5) * 1E5 |  floor / 1E5), lon: (.Longitude | (tonumber + 0.5) * 1E5 |  floor / 1E5)}) + (.Place | {node: .NptgLocalityRef}) + (.StopClassification | {stoptype: .StopType}) | del(.[] | select(. == null))' > naptandata.ndjson
 
 < naptandata.ndjson jq -c -s -r '.' > naptandata.json
 
