eb97168dedafdcb6c000b6ce13a27752a52120ee
Will Deakin <will.deakin@crinstitute.org.uk>
2018-02-27 08:12:08 +0000

    To reference local instance of suniq.sh script

--- NaPTAN-Rail/04-Model-05-iNterpret/run.sh
+++ NaPTAN-Rail/04-Model-05-iNterpret/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Type	Count" > StopTypes.tsv
-< StopPoint.ndjson jq -c -r '.StopClassification.StopType' | suniq.sh >> StopTypes.tsv
+< StopPoint.ndjson jq -c -r '.StopClassification.StopType' | ./suniq.sh >> StopTypes.tsv
 
 < StopPoint.ndjson jq -c '{AtcoCode} + (.Descriptor | {CommonName, Street, Indicator} ) + (.Place.Location.Translation | {lat: .Latitude, lon: .Longitude}) + (.Place | {node: .NptgLocalityRef}) + (.StopClassification | {stoptype: .StopType}) | del(.[] | select(. == null))' > naptandata.ndjson
 
