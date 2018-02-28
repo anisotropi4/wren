e8152200af2dffefe8d999fa38fb40d9af99efa7
Will Deakin <will.deakin@crinstitute.org.uk>
2018-02-28 19:52:52 +0000

    Fix rounding error on points

--- NaPTAN-Rail/04-Model-05-iNterpret/run.sh
+++ NaPTAN-Rail/04-Model-05-iNterpret/run.sh
@@ -6,8 +6,7 @@
 echo "Type	Count" > StopTypes.tsv
 < StopPoint.ndjson jq -c -r '.StopClassification.StopType' | sort - | uniq -c | sort -rn | sed 's/^ *\([0-9][0-9]*\) \(.*\)$/\2\t\1/' >> StopTypes.tsv
 
-
-< StopPoint.ndjson jq -c '{AtcoCode} + (.Descriptor | {CommonName, Street, Indicator} ) + (.Place.Location.Translation | {lat: (.Latitude | (tonumber + 0.5) * 1E5 |  floor / 1E5), lon: (.Longitude | (tonumber + 0.5) * 1E5 |  floor / 1E5)}) + (.Place | {node: .NptgLocalityRef}) + (.StopClassification | {stoptype: .StopType}) | del(.[] | select(. == null))' > naptandata.ndjson
+< StopPoint.ndjson jq -c '{AtcoCode} + (.Descriptor | {CommonName, Street, Indicator} ) + (.Place.Location.Translation | {lat: (.Latitude | (tonumber * 1E5) + 0.5 |  floor / 1E5), lon: (.Longitude | (tonumber * 1E5) + 0.5 |  floor / 1E5)}) + (.Place | {node: .NptgLocalityRef}) + (.StopClassification | {stoptype: .StopType}) | del(.[] | select(. == null))' > naptandata.ndjson
 
 < naptandata.ndjson jq -c -s -r '.' > naptandata.json
 
