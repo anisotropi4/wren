ed410445abc52020dfad5c5e1a4777c2a9d6b5bc
Will Deakin <guido.eco@crinstitute.org.uk>
2018-06-13 20:55:17 +0100

    With video creation

---
+++ Travel-Time/04-Model/link.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import os, sys
+
+header = True
+with open('stationorder') as file_data:
+    for file_line in file_data:
+        if header:
+            header = False
+            continue
+        (n, crs, _, _) = file_line.rstrip().split('\t')
+        for i in range(1,5):
+            from_file = 'images/topo-{0}-PT_0{1}.png'.format(crs, i)
+            to_file = 'output/topo-{0}-PT_0{1}.png'.format(n, i)
+            os.link(from_file, to_file)
