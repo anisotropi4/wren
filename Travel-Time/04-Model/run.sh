2435daf24731c91b9f93de6f30e1b206cc7715bf
Will Deakin <will.deakin@crinstitute.org.uk>
2019-03-16 22:25:16 +0000

    use frames not output

--- Travel-Time/04-Model/run.sh
+++ Travel-Time/04-Model/run.sh
@@ -20,7 +20,7 @@ done
 
 for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32
 do
-    ln start_0.png.template output/start_${i}.png
+    ln start_0.png.template frames/start_${i}.png
 done
 
-ffmpeg -framerate 6 -pattern_type glob -i 'output/*.png' -c:v libx264 -pix_fmt yuv420p output.mp4
+ffmpeg -framerate 6 -pattern_type glob -i 'frames/*.png' -c:v libx264 -pix_fmt yuv420p output.mp4
