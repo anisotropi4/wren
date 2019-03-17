d861947ddb6471a7c30f20c56a43836a7d277378
Will Deakin <will.deakin@crinstitute.org.uk>
2019-03-17 21:41:08 +0000

    automatic generate images

---
+++ Travel-Time/04-Model/modal.js
@@ -0,0 +1,45 @@
+var images = document.getElementsByTagName('img');
+var nList = {};
+nList = [];
+for(var i = 0; i < images.length; i++) {
+    if(! images[i].src) continue;
+    var thisID = images[i].src.split('\\').pop().split('/').pop().split('.').shift().replace('topo-', '');
+    images[i].id = thisID;
+    images[i].onclick = makeModalClosure(thisID);
+    var this_tag = thisID.split('-').pop();
+    nList[this_tag] = 1;
+}
+
+var markdown = document.getElementsByTagName('article')[0];
+var modal = document.getElementById('Modal');
+var modalImage = document.getElementById('image');
+var captionText = document.getElementById('caption');
+
+function makeModalClosure(thisID) {
+    return function() {
+        openModal(thisID);
+    };
+}
+function openModal(thisID) {
+    var thisImage = document.getElementById(thisID);
+    modal.style.display = "block";
+    modalImage.src = thisImage.src;
+    captionText.innerHTML = thisImage.alt;
+    modalImage.height = window.innerHeight;
+    modal.style.left = (window.innerWidth - modal.clientWidth) / 2 - markdown.offsetTop;
+    var thisCRS = thisImage.id.split('-').shift();
+    for(var n in nList) {
+	var thisImageID = thisCRS + "-" + n;
+	var thisNID = n.split('_').pop();
+        var thumbnail = document.getElementById('thumb-' + thisNID);
+	thumbnail.src = "images/topo-" + thisImageID + ".png";
+	thumbnail.onclick = makeModalClosure(thisImageID);
+	thumbnail.alt = "thumbnail: " + document.getElementById(thisImageID).alt;
+    };
+}
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+    modal.style.display = "none";
+    markdown.style.overflow = "auto";
+}
+
--- Travel-Time/04-Model/run.sh
+++ Travel-Time/04-Model/run.sh
@@ -4,23 +4,45 @@
 for CRS in ABD ADV ASI BAN BBN BDI BDM BFR BHI BHM BIC BKG BMH BMO BMS BNG BON BPN BPW BRE BRI BSK BSW BTH BTN BWK CAR CBE CBG CDF CDQ CHD CHM CHX CLJ CLT CNM COL COV CPM CRE CST CTK CTM CTR DAR DBY DEE DFD DHM DID DKG DON EAL EBF EBN ECR EDB EGR EMD EPS ESL EUS EXC EXD FKC FST GCR GLC GLD GLM GLQ GRA GTW HFD HGS HGT HHE HRW HUD HUL HWN HWY IFD INV IPS KGX KNG LAN LBG LBO LCN LDS LEI LIV LMS LPY LST LUT LVC MAC MAI MAN MBR MCO MCV MDE MIA MKC MYB NBY NCL NMP NNG NOT NRW NTA NUN NWP NXG OXF PAD PBO PLY PMH PMS PNZ POO PRE PTH PUT RAY RDG RDH RMD RMF RUG RUN SAL SCA SEV SHF SHR SLO SNF SOA SOC SOT SOU SOV SPL SPT SRA SSD STA SUR SVG SWA SWI SYL TAU TBD TBW TON TRU TWI VIC VXH WAE WAT WBQ WEY WFJ WGC WGN WIM WIN WKF WNR WOK WOS WVH YRK
 do
 
-    for i in PT_01 PT_02 PT_03 PT_04 
+    for i in PT_01 PT_02 PT_03 PT_04 HW_01 HW_02 HW_03
     do
         if [ -f ../03-Explore/output/topo-${CRS}-${i}.svg -a ! -f images/topo-${CRS}-${i}.png ]
         then
             svgexport ../03-Explore/output/topo-${CRS}-${i}.svg  /tmp/topo-${CRS}-${i}.png.$$
-            convert /tmp/topo-${CRS}-${i}.png.$$ -background white -flatten images/topo-${CRS}-${i}.png
+            convert /tmp/topo-${CRS}-${i}.png.$$ -background white -flatten frames/topo-${CRS}-${i}.png
+	    convert /tmp/topo-${CRS}-${i}.png.$$ -transparent white images/topo-${CRS}-${i}.png
         fi
     done
 done
 
-./html-script.sh > index.html
+#./html-script.sh > index.html
 
-./link.py
+if [ ! -f output.mp4 ]; then
+    ./link.py
 
-for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32
-do
-    ln start_0.png.template frames/start_${i}.png
-done
+    for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32
+    do
+	ln start_0.png.template frames/start_${i}.png
+    done
+    
+    ffmpeg -framerate 6 -pattern_type glob -i 'frames/*.png' -c:v libx264 -pix_fmt yuv420p output.mp4
+fi
 
-ffmpeg -framerate 6 -pattern_type glob -i 'frames/*.png' -c:v libx264 -pix_fmt yuv420p output.mp4
+if [ ! -f output.html ]; then
+   # create output.html file based 
+   scrape -e 'article' head.html | sed 's/[\t ][\t ]*$//' > 02-head-article.html
+   grep -Fwvxf head-article.html head.html > 01-head-main.html
+
+   sed -i '/^\s*$/d' 01-head-main.html
+   tail -1 01-head-main.html > 06-head-main.html
+   sed -i '$d' 01-head-main.html
+
+   tail -1 02-head-article.html > 05-head-article.html
+   sed -i '$d' 02-head-article.html
+
+   ./travel-time-01.py > body.yaml
+   output-html.py --yaml body.yaml body.html
+   scrape -e table body.html | sed 's/[\t ][\t ]*$//' > 03-body-table.html
+
+   cat 01-head-main.html 02-head-article.html 03-body-table.html 04-end-main.html 05-head-article.html 06-head-main.html > output.html
+fi
