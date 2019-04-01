b3f6c8f6adb04c241c7ebb985ecd5d8576ee8845
Will Deakin <will.deakin@crinstitute.org.uk>
2019-04-01 21:48:02 +0100

    refactor render legend scripts

--- Travel-Time/03-Explore/legend.js
+++ Travel-Time/03-Explore/legend.js
@@ -1,17 +1,36 @@
-function update_svg(mTime, nCell) {
-    var quantize = d3.scaleSequential([30, mTime], function(t) { return d3.interpolateViridis(1.0 - t)});
+function setSVGheight(h) {
+  console.log(h);
+  var thisSVG = document.getElementById('legend-svg').firstElementChild;
+  thisSVG.setAttribute('height', h);
+  return h;
+}
 
-    var svg = d3.select("svg");      
-    svg.append("g")
-	.attr("class", "legend")
-	.attr("transform", "translate(20, 20)");
+function setSVGunit(u) {
+  var thisSVG = document.getElementById('legend-txt');
+  thisSVG.setAttribute('x', 64);
+  thisSVG.setAttribute('y', 15);
+  thisSVG.textContent = 'Time [' + u + ']';
+  return true;
+}
 
-    var legend = d3.legendColor()
-	.labelFormat(d3.format("d"))
-	.labelOffset(40)
-	.cells(nCell)
-	.scale(quantize);
+function updateSVG(mTime, mDuration) {
+  var quantize = d3.scaleSequential([mDuration, mTime], function(t) { return d3.interpolateViridis(1.0 - t)});
+  var nCell = mTime / mDuration;
+  var svg = d3.select("svg");      
+  svg.append("g")
+	  .attr("class", "legend")
+	  .attr("transform", "translate(20, 20)");
 
-    svg.select(".legend")
-	.call(legend);
+  var fString = "d";
+  if (mDuration < 1.0) {
+    fString = ".01f";
+  }
+  var legend = d3.legendColor()
+	    .labelFormat(d3.format(fString))
+	    .labelOffset(30)
+	    .cells(nCell)
+	    .scale(quantize);
+  svg.select(".legend")
+	  .call(legend);
+  return true;
 }
--- Travel-Time/03-Explore/render.js
+++ Travel-Time/03-Explore/render.js
@@ -1,33 +1,38 @@
-const header = `<?xml version="1.0" encoding="utf-8"?>\
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">`;
+function svgLegendClosure(legend, parameters) {
+  var [mTime, mDuration, mHeight, mUnit] = parameters;
+  return (async function() {
+	  const instance = await phantom.create();
+	  const page = await instance.createPage();
+	  await page.property('viewportSize', { width: 100, height: 600 });
+	  const status = await page.open('legend.html');
+	  console.log(`Page opened with status [${status}].`);
+	  const fs = require('fs');
+    console.log(`Set SVG height ${mHeight}.`);
 
-function svgClosure(legend) {
-    return (async function() {
-	const instance = await phantom.create();
-	const page = await instance.createPage();
-	await page.property('viewportSize', { width: 100, height: 600 });
-	const status = await page.open(`${legend}_legend.html`);
-	console.log(`Page opened with status [${status}].`);
-	const fs = require('fs');
-	await page.evaluate(function() {
-	    return document.getElementById('legend-svg').innerHTML;
-	}).then(function(html){
+    await page.evaluate(function(h) {
+      setSVGheight(h);      
+    }, mHeight);    
+    console.log(`Set time unit ${mUnit}.`);
+    await page.evaluate(function(u) {
+      setSVGunit(u);
+    }, mUnit);
+    console.log(`Render SVG legend ${mTime} in ${mDuration} ${mUnit} blocks.`);
+    await page.evaluate(function(t, n) {
+      updateSVG(t, n);
+      return document.getElementById('legend-svg').innerHTML;
+    }, mTime, mDuration).then(function(html){
 	    console.log(`Writing ${legend}_legend.svg`);
 	    var fp = fs.createWriteStream(`${legend}_legend.svg`, {'flags': 'w+'});
-	    fp.write(header, (err) => {
-		if (err) throw err;
-	    });
 	    fp.write(html, (err) => {
-		if (err) throw err;
+		    if (err) throw err;
 	    });
-	});
-	await instance.exit();
-    })();
+	  });
+	  await instance.exit();
+  })();
 }
 
 const phantom = require('phantom');
-const legends = ["HW", "PT"];
-for (var i=0; i < legends.length; i++) {
-    var thisLegend = legends[i];
-    svgClosure(thisLegend);
+const legends = {"HW": [450, 30, 340, 'm'], "PT": [720, 30, 530, 'm'], "HW2": [7.5, 0.5, 340, 'h'], "PT2": [12, 0.5, 530, 'h']};
+for (var k in legends) {
+  svgLegendClosure(k, legends[k]);
 }
